d8d93c7f037039a21aa6e96560b55477ea728bff
OlehMalskyi <omalskij3@gmail.com>
2023-10-31 19:30:02 +0200

    lb6.1_rek

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB6.1 REK/LB6.1_REK.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int SIZE = 20;
+			int arr[SIZE];
+			create(arr, SIZE, -20, 34, 0);
+			Assert::AreEqual(-26, Sum(arr, SIZE, 0, 0), 0.00001);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			const int SIZE = 20;
+			int arr[SIZE];
+			create(arr, SIZE, -20, 34, 0);
+			Assert::AreEqual(3, Count(arr, SIZE, 0, 0), 0.00001);
+		}
+	};
+}
