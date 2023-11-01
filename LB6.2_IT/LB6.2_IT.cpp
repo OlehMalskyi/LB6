9f4071bf043cc703f58e0ee4522a2c4146ea4dca
OlehMalskyi <omalskij3@gmail.com>
2023-11-02 00:15:48 +0200

    lb6.1_it

---
+++ LB6.2_IT/LB6.2_IT.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+void create(int* arr, int SIZE, int MIN, int MAX)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        arr[i] = MIN + rand() % (MAX - MIN + 1);
+    }
+}
+
+void print(int* arr, int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int findMinNum(int* arr, int SIZE, int min)
+{
+    int minNum = min;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (minNum > arr[i]) {
+            minNum = arr[i];
+        }
+    }
+    return minNum;
+}
+
+int findMaxNum(int* arr, int SIZE, int max)
+{
+    int maxNum = max;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (maxNum < arr[i]) {
+            maxNum = arr[i];
+        }
+    }
+    return maxNum;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int MIN, MAX, SIZE;
+
+    cout << "Введіть розмір масиву (SIZE): "; cin >> SIZE;
+    cout << "Введіть діапазон елементів масиву (MIN): "; cin >> MIN;
+    cout << "Введіть діапазон елементів масиву (MAX): "; cin >> MAX;
+    cout << endl;
+
+    int* arr = new int[SIZE];
+    create(arr, SIZE, MIN, MAX);
+
+
+    print(arr, SIZE);
+    cout << "\n\n";
+    cout << "Мінімальне число = " << findMinNum(arr, SIZE, MAX) << endl;
+    cout << "Максимальне число = " << findMaxNum(arr, SIZE, MIN) << endl;
+    cout << endl;
+    cout << "Сума мінімального та максимального числа: " << findMinNum(arr, SIZE, MAX) + findMaxNum(arr, SIZE, MIN) << endl;
+
+    delete[] arr;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB6.2_IT/LB6.2_IT.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int SIZE = 20;
+			int arr[SIZE]{};
+			int MIN = -29;
+			int MAX = 30;
+			create(arr, SIZE, MIN, MAX);
+			Assert::AreEqual(-28, findMinNum(arr, SIZE, MAX), 0.0001);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			const int SIZE = 20;
+			int arr[SIZE]{};
+			int MIN = -29;
+			int MAX = 30;
+			create(arr, SIZE, MIN, MAX);
+			Assert::AreEqual(27, findMaxNum(arr, SIZE, MIN), 0.0001);
+		}
+	};
+}
