14518f9ec5c98e32b4c3bab01fac00393fde930c
OlehMalskyi <omalskij3@gmail.com>
2023-11-02 00:20:41 +0200

    lb6.2_rek

---
+++ LB6.2_REK/LB6.2_REK.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+void create(int* arr, int SIZE, int MIN, int MAX, int i)
+{
+    if (i < SIZE)
+    {
+        arr[i] = MIN + rand() % (MAX - MIN + 1);
+        create(arr, SIZE, MIN, MAX, ++i);
+    }
+}
+
+void print(int* arr, int SIZE, int i)
+{
+    if (i < SIZE)
+    {
+        cout << arr[i] << ' ';
+        print(arr, SIZE, ++i);
+    }
+}
+
+int findMinNum(int* arr, int SIZE, int min, int i)
+{
+    if (i < SIZE)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+        findMinNum(arr, SIZE, min, ++i);
+    }
+    else
+        return min;
+}
+
+int findMaxNum(int* arr, int SIZE, int max, int i)
+{
+    if (i < SIZE)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+        findMaxNum(arr, SIZE, max, ++i);
+    }
+    else
+        return max;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int MIN, MAX, SIZE;
+
+    cout << "Введіть розмір масиву (SIZE): "; cin >> SIZE;
+    cout << "Введіть діапазон елементів масиву (MIN): "; cin >> MIN;
+    cout << "Введіть діапазон елементів масиву (MAX): "; cin >> MAX;
+    cout << endl;
+
+    int* arr = new int[SIZE];
+    create(arr, SIZE, MIN, MAX, 0);
+
+
+    print(arr, SIZE, 0);
+    cout << "\n\n";
+    cout << "Мінімальне значення = " << findMinNum(arr, SIZE, MAX, 0) << endl;
+    cout << "Максимальне значення = " << findMaxNum(arr, SIZE, MIN, 0) << endl;
+    cout << endl;
+    cout << "Сума мінімального та максимального числа: " << findMinNum(arr, SIZE, MAX, 0) + findMaxNum(arr, SIZE, MIN, 0) << endl;
+
+    delete[] arr;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB6.2_REK/LB6.2_REK.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int SIZE = 20;
+			int arr[SIZE]{};
+			int MIN = -29;
+			int MAX = 30;
+			int i = 0;
+			create(arr, SIZE, MIN, MAX, 0);
+			Assert::AreEqual(-28, findMinNum(arr, SIZE, MAX, 0), 0.0001);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			const int SIZE = 20;
+			int arr[SIZE]{};
+			int MIN = -29;
+			int MAX = 30;
+			int i = 0;
+			create(arr, SIZE, MIN, MAX, 0);
+			Assert::AreEqual(27, findMaxNum(arr, SIZE, MIN, 0), 0.0001);
+		}
+	};
+}
