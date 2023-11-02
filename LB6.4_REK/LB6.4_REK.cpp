7516002c509425c26fffdd055bc0d51bd44890d4
OlehMalskyi <omalskij3@gmail.com>
2023-11-02 18:17:17 +0200

    lb6.4_rek

---
+++ LB6.4_REK/LB6.4_REK.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+void create(int* arr, int SIZE, int MIN, int MAX, int i)
+{
+    if (i < SIZE)
+    {
+        arr[i] = MIN + rand() % (MAX - MIN + 1);
+        create(arr, SIZE, MIN, MAX, ++i);
+    }
+}
+
+void print(int* arr, int SIZE, int i)
+{
+    if (i < SIZE)
+    {
+        cout << arr[i] << ' ';
+        print(arr, SIZE, ++i);
+    }
+}
+
+int count(int* arr, int SIZE, int nCount, int C, int i)
+{
+    if (i < SIZE)
+    {
+        if (arr[i] > C)
+        {
+            nCount++;
+        }
+        count(arr, SIZE, nCount, C, ++i);
+    }
+    else return nCount;
+}
+
+double mult(int* arr, int SIZE, int max, int index, double mul, int i)
+{
+    if (i < SIZE)
+    {
+        if (abs(arr[i]) > max)
+        {
+            max = abs(arr[i]);
+            index = i;
+        }
+        mult(arr, SIZE, max, index, mul, ++i);
+    }
+    else {
+        for (int i = index + 1; i < SIZE; i++)
+        {
+            mul *= arr[i];
+        }
+        return mul;
+    }
+}
+
+void sort(int* arr, const int SIZE, int i)
+{
+    int min = arr[i];
+    int imin = i;
+    for (int j = i + 1; j < SIZE; j++)
+        if (min > arr[j])
+        {
+            min = arr[j];
+            imin = j;
+        }
+    arr[imin] = arr[i];
+    arr[i] = min;
+
+    if (i < SIZE - 2)
+        sort(arr, SIZE, i + 1);
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double MIN, MAX, C;
+    int SIZE;
+
+    cout << "Введіть розмір масиву (SIZE): "; cin >> SIZE;
+    cout << "Введіть діапазон елементів масиву (MIN): "; cin >> MIN;
+    cout << "Введіть діапазон елементів масиву (MAX): "; cin >> MAX;
+    cout << "С = "; cin >> C;
+    cout << endl;
+
+    int* arr = new int[SIZE];
+
+    create(arr, SIZE, MIN, MAX, 0);
+
+    print(arr, SIZE, 0);
+    cout << endl;
+    cout << "count = " << count(arr, SIZE, 0, C, 0) << endl;
+    cout << "Добуток елементів масиву після макс.числа за модулем: " << mult(arr, SIZE, arr[0], 0, 1, 0) << endl;
+    sort(arr, SIZE, 0);
+    print(arr, SIZE, 0);
+
+    delete[] arr;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB6.4_REK/LB6.4_REK.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int SIZE = 20;
+			int arr[SIZE]{};
+			int MIN = -20;
+			int MAX = 20;
+			int C = 2;
+			create(arr, SIZE, MIN, MAX, 0);
+			Assert::AreEqual(8, count(arr, SIZE, 0, C, 0), 0.00001);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			const int SIZE = 20;
+			int arr[SIZE]{};
+			int MIN = -20;
+			int MAX = 20;
+			create(arr, SIZE, MIN, MAX, 0);
+			Assert::AreEqual(0, mult(arr, SIZE, arr[0], 0, 1, 0), 0.00001);
+		}
+	};
+}
